2095692ffaf03c652c7b5cec7a473ebeb95fdbd4
Roman-Ojha <80277272+roman-ojha@users.noreply.github.com>
2024-04-19 22:21:53 +0545

    feat(user_identity): defined registered function to get form data from user

--- Projects/user_identity.c
+++ Projects/user_identity.c
@@ -45,3 +45,117 @@ int main() {
   app();
   return 0;
 }
+
+void app() {
+  printf("=================== User Identity ======================\n");
+  printf("Choose bellow options\n");
+option:
+  printf("1. Register New User\n");
+  printf("2. Login Registered User\n");
+  int option = 0;
+  scanf("%d", &option);
+  switch (option) {
+  case 1:
+    registerUser();
+    break;
+  case 2:
+    loginUser();
+    break;
+  default:
+    printf("============= Warning: Please chose between given option numbers "
+           "===============\n");
+    goto option;
+    break;
+  }
+}
+
+int writeUserToFile(struct User *user) {
+  FILE *file = fopen("user_data.bin", "ab");
+  if (file == NULL) {
+    printf("Unable to open file for writing.\n");
+    fclose(file);
+    return 0;
+  }
+  int flag = 0;
+  flag = fwrite(user, sizeof(struct User), 1, file);
+  if (flag) {
+    fclose(file);
+    return 1;
+  } else {
+    printf("Error Writing new user to file!\n");
+    fclose(file);
+    return 0;
+  }
+}
+
+struct UserNode *readUsersFromFile() {
+  struct UserNode *head;
+  head = NULL;
+  FILE *file = fopen("user_data.bin", "rb");
+  if (file == NULL) {
+    // printf("Unable to open file for writing.\n");
+    fclose(file);
+    return head;
+  }
+  struct User user;
+  while (fread(&user, sizeof(struct User), 1, file)) {
+    head = userListPush(head, user);
+  }
+  fclose(file);
+  return head;
+}
+
+int doesEmailExist(struct UserNode *userHead, char email[]) {
+  struct UserNode *ptr = userHead;
+  while (ptr != NULL) {
+    if (strcmp(email, ptr->user.email) == 0)
+      return 1;
+    ptr = ptr->next;
+  }
+  return 0;
+}
+
+int registerUser() {
+  char password[50], c_password[50];
+  struct DOB dob;
+  struct User user;
+  struct Location location;
+  system("cls || clear");
+  printf("=================== User Registration ======================\n");
+  printf("Enter First Name: ");
+  scanf("%s", user.first_name);
+  printf("Enter Last Name: ");
+  scanf("%s", user.last_name);
+email:
+  printf("Enter Email: ");
+  scanf("%s", user.email);
+  printf("Enter Height: ");
+  scanf("%f", &user.height);
+  printf("Enter Place: ");
+  scanf("%s", location.place);
+  printf("Enter District: ");
+  scanf("%s", location.district);
+password:
+  printf("Enter Password: ");
+  scanf("%s", password);
+  printf("Enter Confirm Password: ");
+  scanf("%s", c_password);
+  if (strcmp(password, c_password) != 0) {
+    printf("============== Warning: Password didn't match please re-enter "
+           "password & confirm password ==================\n");
+    goto password;
+  } else {
+    strcpy(user.password, password);
+    goto next;
+  }
+next:
+  printf("Enter Date of birth Year: ");
+  scanf("%d", &dob.year);
+  printf("Enter Date of birth Month: ");
+  scanf("%d", &dob.month);
+  printf("Enter Date of birth Day: ");
+  scanf("%d", &dob.day);
+  user.location = location;
+  user.date_of_birth = dob;
+  return 1;
+}
